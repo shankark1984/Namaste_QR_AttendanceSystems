f14dfaceceaf9dda39268522c570a9d37095ada2
shankark1984 <46882965+shankark1984@users.noreply.github.com>
2024-08-29 01:16:34 +0530

    ok

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // Constants
-const MAX_DISTANCE_KM = 5; // Approximately 100 meters
-const SCAN_DELAY = 5000;
+const MAX_DISTANCE_KM = 1; // Approximately 100 meters
+const SCAN_DELAY = 500000;
 const API_KEY = 'AIzaSyDKPxKSID_Vq7TVXexqbvlbzjffSKkBsDA';
 const SHEET_ID = '1CzaJwL1YLvKqBVn2l2wLIxAUKO1U0jYMIpo5_RgYC-E';
 const RANGE = 'Attendance!A1:I';
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'namaste-v2';
+const CACHE_NAME = 'namaste-v5';
 const urlsToCache = [
     '/',
     '/index.html',
